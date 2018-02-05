b7aaea636980eae8848b165ad607af599e7c7358
shabib87 <shabib.sust@gmail.com>
2018-02-06 00:37:33 +0600

    Algorithm to transpose major and minor chords with basic GUI completed.

---
+++ Key Transpose/ViewController.swift
@@ -0,0 +1,134 @@
+//
+//  ViewController.swift
+//  Key Transpose
+//
+//  Created by Ahmad Shabibul Hossain on 2/5/18.
+//  Copyright © 2018 Ahmad Shabibul Hossain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let allMajorKeys = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+    private let allMinorKeys = ["Am", "A#m", "Bm", "Cm", "C#m", "Dm", "D#m", "Em", "Fm", "F#m", "Gm", "G#m"]
+    
+    fileprivate var selectedKeyIndex = 0
+    
+    @IBOutlet private var majorField: UITextField!
+    @IBOutlet private var majorPicker: UIPickerView!
+    @IBOutlet private var majorKeyInputView: UIView!
+    @IBOutlet private var majorFieldBar: UIToolbar!
+    @IBOutlet private var majorKeySwitch: UISwitch!
+    @IBOutlet private var chordProgressionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        majorField.inputView = majorKeyInputView
+        majorField.inputAccessoryView = majorFieldBar
+        let keys = transposeNotesForMajorKey(key: "C", transposeBy: 0)
+        chordProgressionLabel.text = keys.joined(separator: "\n")
+    }
+    
+    @IBAction private func doneAction(_: Any) {
+        let keyIndex = selectedKeyIndex
+        let keys:[String]!
+        let key: String!
+        if majorKeySwitch.isOn {
+            key = allMajorKeys[keyIndex]
+            keys = transposeNotesForMajorKey(key: key, transposeBy: 0)
+        } else {
+            key = allMinorKeys[keyIndex]
+            keys = transposeNotesForMinorKey(key: key, transposeBy: 0)
+        }
+        majorField.text = key
+        chordProgressionLabel.text = keys.joined(separator: "\n")
+        majorField.resignFirstResponder()
+    }
+    
+    @IBAction private func majroSwitchValueChangeAction(_: Any) {
+        majorPicker.reloadAllComponents()
+    }
+    
+    private func transposeNotesForMinorKey(key: String, transposeBy step: Int) -> [String] {
+        let index = allMinorKeys.index(of: key)!
+        
+        // all major chords for a key are +1, +3, +8, +10 index of the key in major key array
+        let firstMajorOfCycle = rotatedIndexForIndex(index: (index + 1 + step))
+        let secondMajorOfCycle = rotatedIndexForIndex(index: (index + 3 + step))
+        let thirdMajorOfCycle = rotatedIndexForIndex(index: (index + 8 + step))
+        let fourthMajorOfCycle = rotatedIndexForIndex(index: (index + 10 + step))
+        
+        // all minor chords for a key are +5, +7 index of the key in major key array
+        let firstMinorOfCycle = rotatedIndexForIndex(index: (index + step))
+        let secondMinorOfCycle = rotatedIndexForIndex(index: (index + 5 + step))
+        let thirdMinorOfCycle = rotatedIndexForIndex(index: (index + 7 + step))
+        
+        return [allMinorKeys[firstMinorOfCycle],
+                allMajorKeys[secondMajorOfCycle],
+                allMajorKeys[thirdMajorOfCycle],
+                allMajorKeys[fourthMajorOfCycle],
+                allMinorKeys[secondMinorOfCycle],
+                allMinorKeys[thirdMinorOfCycle],
+                allMajorKeys[firstMajorOfCycle]]
+    }
+    
+    private func transposeNotesForMajorKey(key: String, transposeBy step: Int) -> [String] {
+        let index = allMajorKeys.index(of: key)!
+        
+        // all major chords for a key are -2(+10), +5, +7 index of the key in major key array
+        let firstMajorOfCycle = rotatedIndexForIndex(index: (index + step))
+        let secondMajorOfCycle = rotatedIndexForIndex(index: (index + 5 + step))
+        let thirdMajorOfCycle = rotatedIndexForIndex(index: (index + 7 + step))
+        let fourthMajorOfCycle = rotatedIndexForIndex(index: (index + 10 + step))
+        
+        // all minor chords for a key are -3(+9), +2, +4 index of the key in major key array
+        let firstMinorOfCycle = rotatedIndexForIndex(index: (index + 9 + step))
+        let secondMinorOfCycle = rotatedIndexForIndex(index: (index + 2 + step))
+        let thirdMinorOfCycle = rotatedIndexForIndex(index: (index + 4 + step))
+        
+        return [allMajorKeys[firstMajorOfCycle],
+                allMinorKeys[firstMinorOfCycle],
+                allMajorKeys[secondMajorOfCycle],
+                allMajorKeys[thirdMajorOfCycle],
+                allMinorKeys[secondMinorOfCycle],
+                allMinorKeys[thirdMinorOfCycle],
+                allMajorKeys[fourthMajorOfCycle]]
+    }
+    
+    func rotatedIndexForIndex(index: Int) -> Int {
+        if index >= allMajorKeys.count {
+            return index - allMajorKeys.count
+        } else if index < 0 {
+            return allMajorKeys.count + index
+        }
+        return index
+    }
+}
+
+extension ViewController: UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return allMajorKeys.count
+    }
+}
+
+extension ViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if majorKeySwitch.isOn {
+            return allMajorKeys[row]
+        } else {
+            return allMinorKeys[row]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedKeyIndex = row
+    }
+}
+
